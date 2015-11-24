f71298a13c2419cb51e18ed8f9e5a3e1a4dd7886
Bikash Dash(0xfb) <bikash.nit.12@gmail.com>
2015-11-24 17:50:12 +0530

    shellcode_bin

--- Shellcode_bin.py
+++ Shellcode_bin.py
@@ -1,9 +1,10 @@
 import os
+#this will convert shellcode to bin file for shellcode analysis
 print "*****Shellcode To Hex ANd Bin****"
 from binascii import unhexlify as unhx
 
 encoded = open('shell.txt').read() # The shellcode dump
-out = open('shellcode.txt', 'wb')
+out = open('shellcode.bin', 'wb')
 
 for s in encoded.split('%'):
     if len(s) == 5:
